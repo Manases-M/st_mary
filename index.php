90023f55dfd8358cf4d1ab9d9fe9d0cfd3a822b2
Manases-M <mwithukiachege@gmail.com>
2021-08-04 22:54:21 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>st_mary</title>
+    <!-- linking css -->
+     <link rel="stylesheet" href="css/bootstrap.min.css">
+     <!-- linking fontawesome -->
+     <script src="https://kit.fontawesome.com/6b7a64a74f.js" crossorigin="anonymous"></script>
+     <!-- custom css -->
+     <link rel="stylesheet" href="css/style.css">
+     
+</head>
+<body>
+
+<!-- nav bar starts -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-success">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">
+        <img src="image/logo.png" alt="st mary">
+       St Mary's
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
+      <div class="navbar-nav ms-auto me-3">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </div>
+    </div>
+  </div>
+</nav>
+<!-- nav bar ends -->
+
+<!-- slider starts -->
+<div id="carouselExampleCaptions" class="carousel slide slider" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image/c4.webp" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Priviledge your child </h5>
+        <p>We bring out the best in every child</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="image/c2.webp" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Subtitutional learning</h5>
+        <p>We make learning easier for every one.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="image/c1.webp" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block ">
+        <h5>Best learning enviroment</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<!-- slider ends -->
+
+<!--marketing section starts  -->
+<section class="marketingsection text-secondary">
+  <center>
+  <h5>This is what we offer</h5>
+  <div class="border"></div>
+  <p class="py-2"> 
+    what we offer the best  dummy text of the printing  and
+     typesetting industry.  Lorem Ipsum has been the industry
+</p>
+
+<!-- row starts  -->
+<div class="container pt-sm-2 pt-md-3 pt-lg-5">
+  <div class="row">
+    <!-- first column starts -->
+    <div class="marketing-item p-sm-2 p-md-5  col-md-12 col-lg-4">
+    <i class="fas fa-hands-helping"></i>
+      <h4>Brighter Future</h4>
+      <div class="border"></div>
+        <p>, but also the leap into electronic typesetting,
+           remaining essentially unchanged. It was popularised in 
+           the 1960s with the release of Letraset sheets containing
+            
+
+</p>
+      </div>
+    
+    <!-- second column ends -->
+
+<!-- second column starts -->
+<div class="marketing-item p-sm-2 p-md-5 ol-md-12 col-lg-4">
+<i class="fas fa-people-carry"></i>
+  <h4>Brighter future</h4>
+  <div class="border"></div>
+  <p>but also the leap into electronic typesetting,
+           remaining essentially unchanged. It was popularised in 
+           the 1960s with the release of Letraset sheets containing
+            
+</p>
+
+</div>
+<!-- second column ends -->
+
+<!-- third column starts -->
+<div class="marketing-item p-sm-2 p-md-5 col-md-12 col-lg-4">
+<i class="far fa-bookmark"></i>
+  <h4>What we offer</h4>
+  <div class="border"></div>
+  <p>but also the leap into electronic typesetting,
+           remaining essentially unchanged. It was popularised in 
+         
+</p>
+
+</div>
+<!-- third column ends -->
+  </div>
+</div>
+<!-- row ends -->
+</center>
+</section>
+<!-- marketting section ends -->
+
+    <!-- jquery -->
+    <script src="js/jquery-3.6.0.min.js"></script>
+     <!-- boostrap bundle -->
+     <script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
